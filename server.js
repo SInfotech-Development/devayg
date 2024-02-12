32f7b1623d760607a83bd77eee950e6cde2f2c5e
SInfotech-Development <90300054+SInfotech-Development@users.noreply.github.com>
2024-02-12 19:51:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,226 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const axios = require("axios");
+const path = require("path");
+const mysql = require("mysql2");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
 
-app.use(cors());
+const allowedOrigins = [
+  "http://18.223.93.100",
+  "http://localhost:3000",
+  "http://3.130.189.92",
+  "https://www.megasails.com/",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.office365.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "broker@megasails.com",
+    pass: "Mega#$2023",
+  },
+});
+app.use(bodyParser.json());
+
+app.post("/send-email-yachtdetails", async (req, res) => {
+  try {
+    const { recipientEmail, subject, message } = req.body;
+
+    if (!recipientEmail || !subject || !message) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
+
+    const {
+      NM_firstName,
+      NM_lastName,
+      ID_email,
+      NO_phoneNumber,
+      CA_category,
+      DS_comments1,
+      NM_docId,
+    } = message;
+
+    const mailOptions = {
+      from: "broker@megasails.com",
+      to: recipientEmail,
+      subject: "MegaSails- Yacht Broker Services",
+      text: `
+Dear ${NM_firstName} ${NM_lastName},
+
+Thank you for choosing MegaSails for your yacht broker services. We are delighted to inform you that we have received your request, and our dedicated Yacht Service Team will promptly contact you to coordinate and schedule your yacht service.
+
+Your satisfaction is our priority, and we look forward to providing you with top-notch service.
+
+If you have any immediate questions or concerns, feel free to reach out to us at broker@megasails.com.
+
+Best Regards,
+MegaSails Team
+      `,
+    };
+    const mailOptions1 = {
+      from: "broker@megasails.com",
+      to: "broker@megasails.com",
+      subject: "MegaSails- Yacht Broker Services",
+      text: `
+    Name: ${NM_firstName} ${NM_lastName}
+    Email: ${ID_email}
+    Phone: ${NO_phoneNumber}
+    Category: ${CA_category}
+    Comments: ${DS_comments1}
+    DocId:${NM_docId}
+    Link: http://localhost:3000/yachtdetails/${NM_docId}
+          `,
+    };
+
+    // Send email
+    await transporter.sendMail(mailOptions); //Customer
+    // await transporter.sendMail(mailOptions1); //Mega team
+
+    console.log("Email sent successfully");
+    res.json({ success: true, message: "Email sent successfully" });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json({ error: error.message || "An error occurred" });
+  }
+});
+
+app.post("/send-email-contact", async (req, res) => {
+  try {
+    const { recipientEmail, subject, message } = req.body;
+
+    if (!recipientEmail || !subject || !message) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
+
+    const {
+      NM_firstName,
+      NM_lastName,
+      ID_email,
+      NO_phoneNumber,
+      CA_category,
+      DS_comments1,
+    } = message;
+
+    const mailOptions2 = {
+      from: "broker@megasails.com",
+      to: recipientEmail,
+      subject: "MegaSails- Yacht Contact Inquiry",
+      text: `
+Dear ${NM_firstName} ${NM_lastName},
+
+Thank you for choosing MegaSails for your yacht services. We are delighted to inform you that we have received your request, and our dedicated Yacht Service Team will promptly contact you to coordinate and schedule your yacht service.
+
+Your satisfaction is our priority, and we look forward to providing you with top-notch service.
+
+If you have any immediate questions or concerns, feel free to reach out to us at broker@megasails.com.
+
+Best Regards,
+MegaSails Team
+      `,
+    };
+    const mailOptions3 = {
+      from: "broker@megasails.com",
+      to: "broker@megasails.com",
+      subject: "MegaSails- Yacht Contact Inquiry",
+      text: `
+    Name: ${NM_firstName} ${NM_lastName}
+    Email: ${ID_email}
+    Phone: ${NO_phoneNumber}
+    Category: ${CA_category}
+    Comments: ${DS_comments1}    
+          `,
+    };
+
+    // Send email
+    await transporter.sendMail(mailOptions2); //Customer
+    await transporter.sendMail(mailOptions3); //Mega team
+
+    console.log("Email sent successfully");
+    res.json({ success: true, message: "Email sent successfully" });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json({ error: error.message || "An error occurred" });
+  }
+});
+
+app.post("/send-email-service", async (req, res) => {
+  try {
+    const { recipientEmail, subject, message } = req.body;
+
+    if (!recipientEmail || !subject || !message) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
+
+    const {
+      NM_firstName,
+      NM_lastName,
+      ID_email,
+      NO_phoneNumber,
+      CA_category,
+      DS_comments1,
+    } = message;
+
+    const mailOptions4 = {
+      from: "broker@megasails.com",
+      to: recipientEmail,
+      subject: "MegaSails- Yacht Service Inquiry",
+      text: `
+Dear ${NM_firstName} ${NM_lastName},
+
+Thank you for choosing MegaSails for your yacht ${CA_category} services. We are delighted to inform you that we have received your request, and our dedicated Yacht Service Team will promptly contact you to coordinate and schedule your yacht service.
+
+Your satisfaction is our priority, and we look forward to providing you with top-notch service.
+
+If you have any immediate questions or concerns, feel free to reach out to us at broker@megasails.com.
+
+Best Regards,
+MegaSails Team
+      `,
+    };
+    const mailOptions5 = {
+      from: "broker@megasails.com",
+      to: "broker@megasails.com",
+      subject: "MegaSails- Yacht Service Inquiry",
+      text: `
+    Name: ${NM_firstName} ${NM_lastName}
+    Email: ${ID_email}
+    Phone: ${NO_phoneNumber}
+    Category: ${CA_category}
+    Comments: ${DS_comments1}    
+          `,
+    };
+
+    // Send email
+    await transporter.sendMail(mailOptions4); //Customer
+    await transporter.sendMail(mailOptions5); //Mega team
+
+    console.log("Email sent successfully");
+    res.json({ success: true, message: "Email sent successfully" });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json({ error: error.message || "An error occurred" });
+  }
+});
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("/documents", async (req, res) => {
   try {
