b3c482a813be21388159460933d04f593c1cb459
SInfotech-Development <90300054+SInfotech-Development@users.noreply.github.com>
2024-02-13 17:33:49 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -219,7 +219,7 @@ MegaSails Team
   }
 });
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "client/build")));
